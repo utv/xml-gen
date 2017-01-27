ec066714d404b028cf2c206bcad59d597e841dfa
utv <k9amata@gmail.com>
2017-01-26 20:13:44 -0800

    gets files

--- engine.js
+++ engine.js
@@ -1,30 +1,43 @@
 'use strict';
 
 function readFilesAndDirs() {
-    var fs = require('fs')
-    
-    var files = fs.readdirSync(process.cwd());
-    files.forEach(function(element) {
-        alert(element)
-    }, this)
+  // var fs = require('fs');
+
+  // var files = fs.readdirSync(process.cwd());
+  // files.forEach(function (element) {
+  //   alert(element);
+  // }, this)
 }
 
 function testButtonClicked() {
-    var button = document.querySelector('.form__button');
-    button.addEventListener('click', function() {
-        // alert('hey');
-        var fileAndDirs = readFilesAndDirs()
-    })
+  var button = document.querySelector('.form__button');
+  // button.addEventListener('click', function () {
+  //   // alert('hey');
+  //   var fileAndDirs = readFilesAndDirs();
+  // });
 }
 
 function fileChosen() {
-    var chooser = document.querySelector('.form__file')
-    chooser.addEventListener('change', function() {
-        readFilesAndDirs()
-    })
+  let chooser = document.querySelector('input[type=file]');
+  chooser.addEventListener('change', function() {
+    let fs = require('fs');
+    
+    let numDir = 0;
+    let numFile = 0;
+    let files = this.files;
+    if (files.length === 0) return;
+    for (let i = 0; i < files.length; i++) {
+      if (fs.lstatSync(files[i].path).isDirectory()) 
+        console.log('dir = ' + files[i].path + '\n');
+      else
+        console.log('file = ' + files[i].path + '\n');
+    }
+  });
+  
 }
 
-window.onload = function() {
-    testButtonClicked()
-    fileChosen()
+window.onload = function () {
+  console.log('Hello \n' );
+  testButtonClicked();
+  fileChosen();
 };

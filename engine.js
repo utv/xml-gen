846f2fec35acacb4419cce71028e620289c4d14b
utv <k9amata@gmail.com>
2017-01-28 14:10:27 -0800

    adds menu

--- engine.js
+++ engine.js
@@ -1,42 +1,56 @@
-'use strict';
+'use strict'  
 
 function readFilesAndDirs() {
-  // var fs = require('fs');
+  // var fs = require('fs')
 
-  // var files = fs.readdirSync(process.cwd());
+  // var files = fs.readdirSync(process.cwd())
   // files.forEach(function (element) {
-  //   alert(element);
+  //   alert(element)
   // }, this)
 }
 
-function testButtonClicked() {
-  var button = document.querySelector('.form__button');
-  // button.addEventListener('click', function () {
-  //   // alert('hey');
-  //   var fileAndDirs = readFilesAndDirs();
-  // });
-}
-
 function fileChosen() {
-  let chooser = document.querySelector('input[type=file]');
+  let chooser = document.querySelector('input[type=file]')
   chooser.addEventListener('change', function() {
-    const fs = require('fs');
-    const path = require('path');
+    const fs = require('fs')
+    const path = require('path')
     
-    let files = this.files;
-    if (files.length === 0) return;
+    let files = this.files
+    if (files.length === 0) return
 
     for (let i = 0; i < files.length; i++) {
         console.log('path = ' + files[i].path + 
                     ', dir =' + path.dirname(files[i].path) + 
-                    ', base = ' + path.basename(files[i].path) + '\n');
+                    ', base = ' + path.basename(files[i].path) + '\n')
     }
-  });
+  })
   
 }
 
+function createMenu() {
+  const menuItem = ['xml', 'plist', 'image', 'sql']
+  let menu = document.querySelector('.form__menu')
+  
+  let html = ''
+  for (let i = 0; i < menuItem.length; i++) {
+    let url = menuItem[i] + '.html'
+    let a = document.createElement('a');
+    a.innerText = menuItem[i]
+    a.href = url;
+    let li = document.createElement('li')
+    li.appendChild(a)
+    menu.appendChild(li)
+  }
+
+  
+}
+
+function setupUI() {
+  createMenu()
+  // fileChosen()
+}
+
 window.onload = function () {
-  console.log('Hello \n' );
-  testButtonClicked();
-  fileChosen();
-};
+  setupUI()
+}
+

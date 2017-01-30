a1fb6045bdc926e8af336727bd1f84d64467e77b
utv <k9amata@gmail.com>
2017-01-29 17:16:14 -0800

    include jquery

--- engine.js
+++ engine.js
@@ -1,4 +1,6 @@
-'use strict'  
+'use strict'
+
+// var $ = require('jQuery')
 
 function readFilesAndDirs() {
   // var fs = require('fs')
@@ -11,38 +13,43 @@ function readFilesAndDirs() {
 
 function fileChosen() {
   let chooser = document.querySelector('input[type=file]')
-  chooser.addEventListener('change', function() {
+  chooser.addEventListener('change', function () {
     const fs = require('fs')
     const path = require('path')
-    
+
     let files = this.files
     if (files.length === 0) return
 
     for (let i = 0; i < files.length; i++) {
-        console.log('path = ' + files[i].path + 
-                    ', dir =' + path.dirname(files[i].path) + 
-                    ', base = ' + path.basename(files[i].path) + '\n')
+      console.log('path = ' + files[i].path +
+        ', dir =' + path.dirname(files[i].path) +
+        ', base = ' + path.basename(files[i].path) + '\n')
     }
   })
-  
+
 }
 
 function createMenu() {
   const menuItem = ['xml', 'plist', 'image', 'sql']
   let menu = document.querySelector('.form__menu')
-  
+
   let html = ''
   for (let i = 0; i < menuItem.length; i++) {
     let url = menuItem[i] + '.html'
-    let a = document.createElement('a');
+    let a = document.createElement('a')
     a.innerText = menuItem[i]
-    a.href = url;
+
     let li = document.createElement('li')
     li.appendChild(a)
     menu.appendChild(li)
+    a.onclick = function () {
+      console.log(url)
+      // let $ = require('jQuery')
+      
+    }
   }
 
-  
+
 }
 
 function setupUI() {
@@ -50,7 +57,19 @@ function setupUI() {
   // fileChosen()
 }
 
-window.onload = function () {
+// window.onload = function () {
+//   setupUI()
+// }
+
+
+$(document).ready(function () {
   setupUI()
-}
+  $('.form__page').load('xml.html')
+  // $('.form__page').load('xml.html')
+  var parseString = require('xml2js').parseString;
+  var xml = "<root>Hello xml2js!</root>"
+  parseString(xml, function (err, result) {
+      console.dir(result);
+  });
+})
 
